9b037d8e2a238153fc6b0c32e31f6e3d1b59da5d
bimurzaew <85867193+bimurzaew@users.noreply.github.com>
2022-10-01 18:00:02 +0500

    maked header

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import {Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
      <Routes>
-         <Route path={'/'} element={<Login/>}/>
+         <Route path={'/'} element={<Header />}/>
+         <Route path={'/log'} element={<Login/>}/>
          <Route path={'/register'} element={<Register/>}/>
          <Route path={'/chat'} element={<Chat/>}/>
      </Routes>
