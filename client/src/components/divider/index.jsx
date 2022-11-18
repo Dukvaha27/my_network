c858006457b66a7875131d3b6b8e89e6cc4f47ae
Dukvaha27 <dukvaha1997@mail.ru>
2022-11-18 14:42:40 +0300

    test3

--- client/src/components/divider/index.jsx
+++ client/src/components/divider/index.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Divider = styled.hr`
   ${({ vertical }) => vertical && {
     border: 'none',
-    borderLeft: '1px solid hsla(200, 10%, 50%,100)',
+    borderLeft: '1.5px solid hsla(200, 10%, 50%,100)',
     width: '1px',
     margin: '0 .5rem',
   }};
